fdd6ec29ece64082ec8992c1dd4d5b783980eb54
Nv <130412927+nathanvictor281@users.noreply.github.com>
2023-06-01 18:55:42 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const chatboxElement = document.getElementById("chatbox");
+const userInputElement = document.getElementById("userInput");
+const sendButton = document.getElementById("sendButton");
+
+function appendMessage(message, className) {
+    const messageElement = document.createElement("p");
+    messageElement.className = className;
+    messageElement.innerText = message;
+    chatboxElement.appendChild(messageElement);
+
+    // Automatically scroll to the bottom of the chatbox
+    chatboxElement.scrollTop = chatboxElement.scrollHeight;
+}
+
+async function sendMessage() {
+    const userInput = userInputElement.value;
+    userInputElement.value = "";
+    appendMessage("User: " + userInput, "user-message");
+
+    try {
+        const response = await axios.post("https://api.openai.com/v1/chat/completions", {
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "system", content: "You are a helpful assistant." }, { role: "user", content: userInput }]
+        }, {
+            headers: {
+                "Authorization": "Bearer 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",
+                "Content-Type": "application/json"
+            }
+        });
+
+        const chatbotMessage = response.data.choices[0].message.content;
+        appendMessage("ChatGPT: " + chatbotMessage, "chatbot-message");
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+sendButton.addEventListener("click", sendMessage);
+userInputElement.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        sendMessage();
+    }
+});
